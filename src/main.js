47e80ec602975fccb9cb2fd1969eb8e03094280d
codebug <fan552426811@outlook.com>
2020-06-02 18:01:17 +0800

    add i18n

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import VueI18n from 'vue-i18n'
+import 'element-ui/lib/theme-chalk/index.css'
+import messages from './components/common/i18n'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, { size: 'small' })
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'zh', // 设置地区
+  messages // 设置地区信息
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  i18n,
+  render: (h) => h(App)
 }).$mount('#app')
