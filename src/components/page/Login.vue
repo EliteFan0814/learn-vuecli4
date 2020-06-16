f9251c77096b95f3e48d3aefdc77bb4f2d30ef47
codebug <fan552426811@outlook.com>
2020-06-16 18:04:23 +0800

    fpc add login page

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    11
+  <div class="login-wrap">
+    <div class="login">
+
+    </div>
   </div>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.login-wrap {
+  height: 100%;
+  width: 100%;
+  background-image: url(../../assets/img/login-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  .login {
+  }
+}
+</style>
